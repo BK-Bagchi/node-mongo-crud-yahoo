6813b7f7aaf61bdee39bba57030c023db0685c57
unknown <bkbagchi.dipto@gmail.com>
2025-07-29 11:06:33 +0600

    necessary routes are declared

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ app.listen(port, () => {
   console.log(`App is listening at ${port} port`);
 });
 
+//necessary middlewares
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+//necessary routes
+app.get("/", (req, res) => {
+  // home page
+});
+app.get("/show-contact", (req, res) => {
+  //static page to show contact details
+});
+app.get("/add-contact", (req, res) => {
+  //form page to take contact input from user
+});
+app.post("/add-contact", (req, res) => {
+  //post the contact input into db
+});
+app.get("/update-contact", (req, res) => {
+  //form page to take contact update from user
+});
+app.post("/update-contact", (req, res) => {
+  //post the contact update into db
+});
