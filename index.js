dce165079d940fc0838837801ced0f00f1a3ac1b
unknown <bkbagchi.dipto@gmail.com>
2025-08-03 15:19:01 +0600

    new data can be inserted into db using form

--- index.js
+++ index.js
@@ -35,8 +35,18 @@ app.get("/add-contact", (req, res) => {
   //form page to take contact input from user
   res.render("add-contact");
 });
-app.post("/add-contact", (req, res) => {
+app.post("/add-contact", async (req, res) => {
   //post the contact input into db
+  // const contact_2= Contacts.insertOne({
+  //   first_name: first_name,
+  //   last_name: last_name,
+  //   "db_field_name": "form_field_name"
+  // }) but this can be done in a shorter and simpler way.
+
+  const contact = await Contacts.create(req.body);
+  // in this case "db_field_name" and "form_field_name" should be the same
+  //if i'm not using the const contact anywhere else, directly "await Contacts.create(req.body)" can also be written
+  res.redirect("/");
 });
 app.get("/update-contact/:id", async (req, res) => {
   //form page to take contact update from user
